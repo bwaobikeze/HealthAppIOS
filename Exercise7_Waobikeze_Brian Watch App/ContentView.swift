9b91fdb432e1b986091d5e2da5de98bffb08d866
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-10-29 13:34:04 -0500

    added UI and Auth

--- Exercise7_Waobikeze_Brian Watch App/ContentView.swift
+++ Exercise7_Waobikeze_Brian Watch App/ContentView.swift
@@ -6,16 +6,37 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
+    @State var  heartRate = 0
+    var healthStore = HKHealthStore()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Button(action: {
+                
+            }, label: {
+                Text("❤️").font(.system(size: 40))
+            })
+            Spacer()
+            HStack{
+                Text("\(heartRate)").font(.system(size: 70)).fontWeight(.regular)
+                Text("BPM").font(.headline).foregroundStyle(.red).padding(.vertical, 9)
+            }
+            
+        }.onAppear(perform: {
+            start()
+        })
         .padding()
+    }
+    func start(){
+        authorizeHealKit()
+    }
+    func authorizeHealKit(){
+        let healthKitTypes: Set = [HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!]
+        
+        healthStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes){_,_ in}
+
     }
 }
 
---
+++ Exercise7_Waobikeze_Brian Watch App/colors.swift
@@ -0,0 +1,12 @@
+//
+//  colors.swift
+//  Exercise7_Waobikeze_Brian Watch App
+//
+//  Created by brian waobikeze on 10/29/23.
+//
+import Foundation
+import SwiftUI
+
+struct color{
+    static let darkGrey = Color("DarkGrey")
+}
